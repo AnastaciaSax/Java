d57229fce9e0a6305326151c0442b452b08a24ae
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-10 11:45:22 +0300

    feat: app test complete

---
+++ CalculatorTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3), "2 + 3 = 5");
+    }
+}
