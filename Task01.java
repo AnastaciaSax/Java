e1621f996b3186029e54d5aff62f406b3b63d220
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-10 10:54:50 +0300

    feat: task complete

--- Task01.java
+++ Task01.java
@@ -1,4 +1,9 @@
+/*
+Числа Фибоначчи – это последовательность чисел, в которой два первых числа последовательности равны
+0 и 1, а каждое последующее число равно сумме двух предыдущих.
+Показать на экране все числа Фибоначчи в диапазоне от 0 до 10 000 000
 
+ */
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("A great day to crunch numbers, so shall we? \nFibonacci nums up to 10.000.000:");
