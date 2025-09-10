3d92eb944982ff674cc00ca1cdba9d6c1e61e0d2
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-10 10:43:40 +0300

    feat: task1 complete

---
+++ Task01.java
@@ -0,0 +1,45 @@
+/*
+В переменных х и y хранятся два натуральных числа.
+Создайте программу, выводящую на консоль:  результат целочисленного деления x на y;
+ остаток от деления x на y;  квадратный корень x.
+ */
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter week day number - ");
+        int num = sc.nextInt();
+
+        System.out.print("It's...");
+        String day;
+        switch (num) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "ERROR! Check the number, it's within 7";
+        }
+
+        System.out.println(day);
+    }
+}
